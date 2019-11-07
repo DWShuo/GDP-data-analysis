3b04d9ecab3003339800cb8614880b9a37a0d9eb
dwshuo <errorcodes@tuta.io>
2019-11-06 23:31:36 -0500

    updated industry selection

--- project.R
+++ project.R
@@ -1,14 +1,18 @@
 library("readxl")
 #parse in data for analysis
-section6 <- read_excel("Section6.xlsx",sheet ="T60200A-A")
+compA <- read_excel("Section6.xlsx",sheet ="T60200A-A")
+compB <- read_excel("Section6.xlsx",sheet ="T60200B-A")
+compC <- read_excel("Section6.xlsx",sheet ="T60200C-A")
+compD <- read_excel("Section6.xlsx",sheet ="T60200D-A")
 
-sec6DF <- data.frame(section6)
-sec6DF <- sec6DF[-(1:7),]
-sec6DF <- head(sec6DF,-7)
-sec6DF <- sec6DF[,-c(1,3)]
-names(sec6DF) <- sec6DF[1]
-print(sec6DF)
-
-
+compA <- data.frame(compA)
+compA <- compA[-(1:6),]
+compA <- head(compA,-7)
+compA <- compA[,-c(1,3)]
+compA[1,1] <- "Industry"
+names(compA) <- compA[1,]
+compA <- compA[-1,]
+rownames(compA) <- NULL
+print(compA)
 
 
